446c0627a026a76907bc5668cc3684b31c061efd
CaritoDasilva <caritodasilva@gmail.com>
2018-09-25 21:07:22 -0300

    se prueba nuevamente deploy

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,3 +1,20 @@
-favoriteCodes.addEventListener('click', () => {
-  document.getElementById('body').style.filter.blur = "10px";
-})
+function openModals() {
+  document.getElementById('carito').style.display = "none";
+  favoriteCodes.style.display = "none";
+  myWorld.style.display = "none";
+  letsTalk.style.display = "none";
+  lined.style.display = "none";
+  content.style.display = "none";
+
+};
+
+
+
+function closeModal() {
+  document.getElementById('carito').style.display = "block";
+  favoriteCodes.style.display = "block";
+  myWorld.style.display = "block";
+  letsTalk.style.display = "block";
+  lined.style.display = "block";
+  content.style.display = "block";
+};
