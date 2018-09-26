f6b5736defc5ea53554157ea7b14f5400035a40a
CaritoDasilva <caritodasilva@gmail.com>
2018-09-26 10:22:26 -0300

    se adapta tamaño de modales pero solo en desktop

---
+++ docs/js/app.js
@@ -0,0 +1,20 @@
+function openModals() {
+  document.getElementById('carito').style.display = "none";
+  favoriteCodes.style.display = "none";
+  myWorld.style.display = "none";
+  letsTalk.style.display = "none";
+  lined.style.display = "none";
+  content.style.display = "none";
+
+};
+
+
+
+function closeModal() {
+  document.getElementById('carito').style.display = "block";
+  favoriteCodes.style.display = "block";
+  myWorld.style.display = "block";
+  letsTalk.style.display = "block";
+  lined.style.display = "block";
+  content.style.display = "block";
+};
